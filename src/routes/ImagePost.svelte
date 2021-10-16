8db3f353f2a083761cecc0fad108c626774a27ee
Trirst <abeces968@gmail.com>
2021-10-16 17:10:32 +0700

    Image metadata display in image post

--- src/routes/ImagePost.svelte
+++ src/routes/ImagePost.svelte
@@ -1,7 +1,26 @@
 <script>
+    import { fetchAPI } from "@/static/js/helper";
     import { paths } from "@/static/js/paths";
+    import { onMount } from "svelte";
 
     export let params = {};
+    let imageMetadata = {};
+
+    async function getImageMetadata() {
+        return await fetchAPI(paths.ImageField(params.snowflake));
+    }
+
+    onMount(async () => {
+        imageMetadata = await getImageMetadata();
+    });
 </script>
 
-<img src={paths.ImageFile(params.snowflake)} alt="" />
+<div class="flex flex-row space-x-12">
+    <ul class="pl-2">
+        <li><strong>Snowflake: </strong> {imageMetadata.snowflake}</li>
+        <li><strong>Source: </strong> {imageMetadata.source}</li>
+        <li><strong>Image type: </strong> {imageMetadata.type}</li>
+        <li><strong>Uploaded by: </strong> {imageMetadata.user}</li>
+    </ul>
+    <img src={paths.ImageFile(params.snowflake)} alt="" class="max-w-2xl" />
+</div>
