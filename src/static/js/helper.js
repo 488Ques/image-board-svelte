e0ae0b2a41541bd1600e36376401a8223eeda251
Trirst <abeces968@gmail.com>
2021-10-16 15:24:29 +0700

    Only parse JSON if content length is not 0

--- src/static/js/helper.js
+++ src/static/js/helper.js
@@ -1,6 +1,10 @@
 async function fetchAPI(endpoint, options) {
     const res = await fetch(endpoint, options);
-    const data = await res.json();
+    const contentType = res.headers.get('content-length');
+    let data
+    if (!contentType.includes("0")) {
+        data = await res.json();
+    }
     if (res.ok) {
         return data;
     }
