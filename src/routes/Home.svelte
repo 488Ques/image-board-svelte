a9fc542ddce09588ce92ea093cd4e271ea213458
Trirst <abeces968@gmail.com>
2021-10-29 02:07:20 +0700

    Tips

--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -1,3 +1,20 @@
+<script>
+    function getRandomInt(max) {
+        return Math.floor(Math.random() * max);
+    }
+    let tips = [
+        "When searching images by tags, delimit each tag by a whitespace to match them inclusively",
+        "Good luck!",
+        "Stay strong!",
+        "A page includes at maximum 63 images",
+    ];
+</script>
+
 <svelte:head>
     <title>Home</title>
 </svelte:head>
+
+<div class="min-h-screen flex justify-center items-center text-2xl">
+    <strong class="mr-2">Tip:</strong>
+    {tips[getRandomInt(tips.length)]}
+</div>
